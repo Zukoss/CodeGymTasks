db38d0e3a23c49a8db37d435ee44b26981da26da
Paul <pawelzukowski22@gmail.com>
2020-02-22 23:32:18 +0100

    Next.

---
+++ 1.JavaSyntax/src/pl/codegym/task/task04/task0441/Solution.java
@@ -0,0 +1,13 @@
+package pl.codegym.task.task04.task0441;
+
+
+/* 
+Jakiś taki przeciętny
+*/
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //tutaj wpisz swój kod
+    }
+}
